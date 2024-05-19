20afa8f3da007d394041e0bf3525e71e9cb9f2c3
LINUSOBURA <linusobura@gmail.com>
2024-05-19 11:41:44 +0300

    Added standalone Tailwindcss CLI and configured. Also added a subprocess in manage.py to run a watcher for changes in input.css

---
+++ clockske/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["/home/stewie/Clocks_Ke/clockske/store/templates/store/*.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
