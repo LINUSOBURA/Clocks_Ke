460d7a177de5027bf1d822b452f69885ab26199b
Linus Obura <56161418+LINUSOBURA@users.noreply.github.com>
2024-06-03 22:39:10 +0300

    Update settings.py

--- clockske/settings.py
+++ clockske/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-vj*gf*n2g#p6(k9a03)=r#!7=ec8n12w(r&9p=_62dgd6kw#+*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 SECURE_CROSS_ORIGIN_OPENER_POLICY = 'same-origin-allow-popups'
