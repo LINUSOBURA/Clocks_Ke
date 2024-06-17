51f7e3d3c27a23fc3fbd401744001dcdaf4eb1ae
Linus Obura <56161418+LINUSOBURA@users.noreply.github.com>
2024-06-17 11:11:58 +0300

    Update settings.py (Updating MEDIA_URL, to include STATIC_URL path)

--- clockske/settings.py
+++ clockske/settings.py
@@ -128,7 +128,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
-MEDIA_URL = '/images/'
+MEDIA_URL = STATIC_URL + '/images/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
 # Default primary key field type
